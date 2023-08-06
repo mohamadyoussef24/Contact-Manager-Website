47f8e39d1e22cf607a3b4bb2a1e5c06fce956816
mohamadyoussef24 <mohammadyoussef.cs@gmail.com>
2023-08-07 01:31:10 +0300

    create contact controller with functions get and store

--- ContactManager-backend/app/Http/Controllers/ContactController.php
+++ ContactManager-backend/app/Http/Controllers/ContactController.php
@@ -2,9 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
+use Exception;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+    public function getcontact(){
+        $contact = Contact::all();
+        return response() -> json($contact);
+
+    }
+
+    public function storecontact (Request $request){
+
+        $request -> validate([
+            'name' => 'required|string|max:255',
+            'phone' => 'required|string|max:20',
+            'latitude' => 'required|numeric',
+            'longitude' => 'required|numeric',
+
+        ]);
+
+        try{
+            $contact= new Contact([
+                'name' => $request->name,
+                'phone' =>$request->phone,
+                'latitude' =>$request->latitude,
+                'longitude' =>$request->longitude
+                
+            ]);
+            $contact->save();
+            return response()->json([
+                "message" => "ADD Contact successfully"
+            ]);
+
+        }catch(Exception $e){
+            return response()->json([
+                "message" => $e ,
+                "also"=> "ADD contact fail"
+            ]);
+
+        }
+
+
+
+
+    }
 }
